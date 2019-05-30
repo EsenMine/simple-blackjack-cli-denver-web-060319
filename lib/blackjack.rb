e9249b734b38441f00cbd2b501a8250b8a6a2eea
Miné E <mine_esen@yahoo.ca>
2019-05-30 17:16:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,7 +59,7 @@ def runner
   initial_round
   until initial_round == 21
     hit?(initial_round)
-    display_card_total(initial_round)
+    display_card_total(hit?(initial_round))
     binding.pry
   end
 end_game
